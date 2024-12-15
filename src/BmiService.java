76c6ec271507e7bf71449c918167ad49d6cccb8f
Zaharova Yana <yanamelomano@mail.ru>
2024-12-15 19:37:24 +0300

    InitialCommit

---
+++ src/BmiService.java
@@ -0,0 +1,6 @@
+public class BmiService {
+    public int calculate(double weightInKilo, double heightInMeters) {
+        double bmiIndex = weightInKilo / (heightInMeters * heightInMeters);
+        return (int) bmiIndex;
+    }
+}
